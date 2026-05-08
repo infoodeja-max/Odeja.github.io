f2721de33300f5812de546cafca2870c0dd9a54e
infoodeja-max <info.odeja@gmail.com>
2026-05-08 23:17:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function buyNow(){
-    window.open("https://amazon.in/YOUR-LINK", "_blank");
-}
-
-// MOBILE MENU
-function toggleMenu(){
-    document.getElementById("navMenu").classList.toggle("active");
-}

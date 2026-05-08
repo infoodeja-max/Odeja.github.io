31cb71ede71922c696a2408d81550e64cff3bd9a
infoodeja-max <info.odeja@gmail.com>
2026-05-08 23:22:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function buyNow(){
+    window.open("https://amazon.in/YOUR-LINK", "_blank");
+}
+
+// MOBILE MENU
+function toggleMenu(){
+    document.getElementById("navMenu").classList.toggle("active");
+}
